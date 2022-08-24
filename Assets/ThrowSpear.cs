f932f7102505f8b8828fbda7d18797eea0fae5b2
Jari <zalmanhajari@gmail.com>
2022-08-24 21:29:48 +0800

    Fixed the multiple spear bug and now it is working properly

--- Assets/ThrowSpear.cs
+++ Assets/ThrowSpear.cs
@@ -6,16 +6,48 @@ public class ThrowSpear : MonoBehaviour
 {
     public GameObject spear;
     public Transform shotPoint;
-    public bool hasSpear;
+
+    // Trigger enabled is for enableing the checking of objects inside the trigger
+    public bool triggerEnabled = false;
+    // hasSpear to check if the player has the spear
+    public bool hasSpear = true;
     public int launchForce;
+
+    // countdown before the enabling the trigger again
+    public float countdown;
+    public float timer = 2f;
+
+    public BoxCollider2D bc;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0) && hasSpear)
         {
-            //instantiate a spear
+            triggerEnabled = false;
             Shoot();
+            
+        }
+
+        //start the trigger countdown
+        enablePickup();
+        // if countdown gets to 0 and player does not have the spear, enable the trigger
+        if (countdown<=0 && !hasSpear)
+        {
+            triggerEnabled = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (triggerEnabled)
+        {
+            if (collision.gameObject.layer == LayerMask.NameToLayer("Player Spear"))
+            {
+                Destroy(collision.gameObject);
+                hasSpear = true;
+                triggerEnabled = false;
+            }
         }
     }
 
@@ -26,5 +58,20 @@ public class ThrowSpear : MonoBehaviour
         //give velocity
         newSpear.GetComponent<Rigidbody2D>().velocity = transform.right * launchForce;
         hasSpear = false;
+        
+
+    }
+
+    void enablePickup()
+    {
+        if (hasSpear)
+        {
+            countdown = timer;
+        }
+        else
+        {
+           countdown -= Time.deltaTime;
+        }
+        
     }
 }
