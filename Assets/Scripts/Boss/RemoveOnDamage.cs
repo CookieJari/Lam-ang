c45f659ea7e13aa7e8feea0cd1d142469537a840
Jari <zalmanhajari@gmail.com>
2022-11-08 17:10:02 +0800

    Cleaned up the code for the bossing removing unecessary logs

--- Assets/Scripts/Boss/RemoveOnDamage.cs
+++ Assets/Scripts/Boss/RemoveOnDamage.cs
@@ -9,20 +9,15 @@ public class RemoveOnDamage : MonoBehaviour
     public LayerMask lm;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("col all: \t" + collision.gameObject.layer);
-        Debug.Log(lm.value);
         if ((lm.value & (1 << collision.transform.gameObject.layer)) > 0)
         {
-            Debug.Log("col");
             StartCoroutine(Destroy());
         }
         
     }
     IEnumerator Destroy()
     {
-        Debug.Log("Destorying in few:");
         yield return new WaitForSeconds(destroyDelay);
-        Debug.Log("Destroyed");
         Destroy(go);
     }
 }
