20d7f429e66fcc2b6a0a6875d59ac7671c42fc95
Jari <zalmanhajari@gmail.com>
2022-11-23 17:35:27 +0800

    added health bar for lam-ang

---
+++ Assets/HealthBarScript.cs
@@ -0,0 +1,20 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarScript : MonoBehaviour
+{
+    public Slider slider;
+
+    public void SetHealth(float health)
+    {
+        slider.value = health;
+    }
+
+    public void SetMaxHealth(float health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
+}
