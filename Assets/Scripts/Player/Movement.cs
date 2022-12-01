20fd6a5bdd5e17679a8f5ab9bc2851360844290b
Jari <zalmanhajari@gmail.com>
2022-12-01 16:39:31 +0800

    Added block animations for lam-ang + global light to illuminate spike sprites

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -104,6 +104,7 @@ public class Movement : MonoBehaviour
         // ******************ANIMATIONS***********************************
 
         animator.SetFloat("Speed", Mathf.Abs(x));
+        animator.SetFloat("Direction", x);
         animator.SetFloat("SpeedY", rb.velocity.y);
         animator.SetBool("FacingLeft", facingLeft);
     }
@@ -137,6 +138,9 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetMouseButton(1))
         {
+            //Animation for shields
+            animator.SetBool("Block", true);
+
             //for disabling the movement
             shieldUp = true;
             ms.shieldUp = true;
@@ -148,6 +152,8 @@ public class Movement : MonoBehaviour
         }
         else
         {
+            animator.SetBool("Block", false);
+
             shieldUp = false;
             ms.shieldUp = false;
             ts.shieldUp = false;
