ebc8a12dcb44a2cb0e927861b131b5508b47beb7
asher-lab <asher01000001@gmail.com>
2022-12-14 22:59:52 +0800

    Add cinematics bg music and sample dialogue audio

--- Assets/Scripts/CutScene/Templates/beforePrologue_cutscene.cs
+++ Assets/Scripts/CutScene/Templates/beforePrologue_cutscene.cs
@@ -8,31 +8,46 @@ public class beforePrologue_cutscene : MonoBehaviour
 
 public static bool isCutSceneOn;
 public Animator camAnim;
-
 public Animator Scene4Anim;
 
+[SerializeField]
+AudioSource main_theme;
+[SerializeField]
+AudioSource b4_one;
+[SerializeField]
+AudioSource b4_two;
+
+
 void Start() {
+    // Play bg music
+    main_theme.Play();
+
     StartCoroutine(cutscene1());
 }
 
 IEnumerator cutscene1()
     {
+        // Play first frame dialogue
+        b4_one.Play();
         Time.timeScale = 1f;
         Debug.Log("B4 Prologue: Scene1 starts");
         camAnim.SetBool("cutscene1", true);
-        yield return new WaitForSeconds(6.0f);
+        yield return new WaitForSeconds(23.0f);
         StartCoroutine(cutscene2());
     }
 
 IEnumerator cutscene2()
     {
+        
+        b4_two.Play();
         Debug.Log("B4 Prologue: Scene2 starts");
         camAnim.SetBool("cutscene2", true);
-        yield return new WaitForSeconds(6.0f);
+        yield return new WaitForSeconds(17.0f);
         StartCoroutine(cutscene3());
     }
 IEnumerator cutscene3()
     {
+        main_theme.Stop();
         Debug.Log("B4 Prologue: Scene3 starts");
         camAnim.SetBool("cutscene3", true);
         yield return new WaitForSeconds(6.0f);
@@ -90,4 +105,6 @@ IEnumerator loadlevel()
         SceneManager.LoadScene(2);
         
     }
+
+
 }
