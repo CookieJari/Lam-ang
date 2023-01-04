f1d8f08dbf0fdf46eb37f1542dffecc5eff3bb83
Jari <zalmanhajari@gmail.com>
2023-01-05 01:16:11 +0800

    Pre redefense release

--- Assets/Scripts/SceneSwitcher.cs
+++ Assets/Scripts/SceneSwitcher.cs
@@ -8,6 +8,9 @@ public class SceneSwitcher : MonoBehaviour
     public string switchKey = "E";
     bool playerInsidePortal;
 
+    public Vector2 NextLevelPos;
+    public GameObject go;
+    public GameMaster gm;
 
     private void Update()
     {
@@ -35,8 +38,17 @@ public class SceneSwitcher : MonoBehaviour
         }
     }
 
+    public void AdjustGameMaster(Vector2 nextPos)
+    {
+        go = GameObject.Find("Game Master");
+        gm = go.GetComponent("GameMaster") as GameMaster;
+        gm.lastCheckPointPos = nextPos;
+    }
+
     public void NextLevel()
     {
+        AdjustGameMaster(NextLevelPos);
         SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+        
     }
 }
